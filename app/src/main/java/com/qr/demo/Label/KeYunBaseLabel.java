935dc62c5c6f4b384972ffca620800274b60552a
chenlei <52197521aA>
2018-01-11 18:01:52 +0800

    图片打印

--- app/src/main/java/com/qr/demo/Label/KeYunBaseLabel.java
+++ app/src/main/java/com/qr/demo/Label/KeYunBaseLabel.java
@@ -3,6 +3,7 @@ package com.qr.demo.Label;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 
 import com.qr.demo.R;
 import com.qr.print.PrintPP_CPCL;
@@ -40,7 +41,24 @@ public class KeYunBaseLabel extends BaseLabel {
 
         Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.teilu);
 
-        iPrinter.drawGraphic2(0, topPadding, 80, 80, bitmap);
+        // 获得图片的宽高
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        // 设置想要的大小
+        int newWidth = 80;
+        int newHeight = 80;
+        // 计算缩放比例
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // 取得想要缩放的matrix参数
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // 得到新的图片
+        Bitmap newbm = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix,
+                true);
+
+        iPrinter.drawGraphic(0, topPadding, newbm.getWidth(), newbm.getHeight(), newbm);
 
         printTopPading = topPadding + fontPoint3 + fontPoint2 + 8 * 4 - fontPoint2;
     }
